db873ee52b0c1f503340bb0f1bc756276cc55da5
JoWooSung <ender1412@naver.com>
2021-07-30 15:58:39 +0900

    비밀정보보호

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+const {User} = require("./models/User")
+const config = require("./config/key.js")
+
+//application/x-www-form-unlencoded 정보를 받아주기 위함
+app.use(bodyParser.urlencoded({extended: true}))
+//application/json 정보를 받아주기 위함
+app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://Oldentomato:jowoosung123@examplecluster.g7o5t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+// const { createTrue } = require('typescript')
+mongoose.connect(config.mongoURI,{
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(()=> console.log('MongoDB Connected...'))
   .catch(err=> console.log(err))
 
-app.get('/',(req,res) => res.send('Hello World!'))
+
+
+app.get('/',(req,res) => res.send('Hello World! test'))
+
+
+app.post('/register',(req,res) => {
+  //회원가입할때 필요한 정보들을 client에서 가져오면 
+  //그것들을 데이터베이스에 넣어준다.
+
+  const user = new User(req.body)
+  user.save((err,userInfo)=>{
+    if(err) return res.json({success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })//mongoDB method
+})
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
